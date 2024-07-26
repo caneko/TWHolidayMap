c04019c795fdf28861181d7f91a56f37ec7bf10e
caneko <30125100+caneko@users.noreply.github.com>
2024-07-27 01:16:38 +0800

    refactor(app.R): rename var and function name

--- app.R
+++ app.R
@@ -13,119 +13,130 @@ library('rvest')
 
 CityList <- c("基隆市","臺北市","新北市","桃園市","新竹市","新竹縣","苗栗縣","臺中市","彰化縣","雲林縣","南投縣","嘉義市","嘉義縣","臺南市","高雄市","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","連江縣","金門縣")
 
-UpdateData <- function(){
-    html <- read_html("https://www.dgpa.gov.tw/typh/daily/nds.html")
+GetGovData <- function(){
+    #html <- read_html("https://www.dgpa.gov.tw/typh/daily/nds.html")
     #html <- read_html("./testhtml/twoRow.html")
-    #html <- read_html("./testhtml/tes2.html")
+    html <- read_html("./testhtml/tes2.html")
     html %>%
         html_node('table') %>%
         html_table() -> df_gov
-    row_to_delete <- c()
+
+    RowNotCity <- c()
     for (i in 1:nrow(df_gov)){
         if (!(df_gov[i,1] %in% CityList)){
-            row_to_delete <- c(row_to_delete, i)
+            RowNotCity <- c(RowNotCity, i)
         }
     }
-    df_gov <- df_gov[-row_to_delete,]
+    df_gov <- df_gov[-RowNotCity,]
     df_gov <- df_gov[,-3]
-    #df_gov <- df_gov[-23,-3]
     return(df_gov)
 }
 
-LoadMap <- function(){
+LoadTWMap <- function(){
     min_lon <- 118
     max_lon <- 122.5
     min_lat <- 21
     max_lat <- 26
-    tw_shape <- read_sf(dsn = "./shp/CITY.shp")
-    tw_small <- st_crop(tw_shape, xmin = min_lon, xmax = max_lon, ymin = min_lat, ymax = max_lat)
-    return (tw_small)
+    map_shp <- read_sf(dsn = "./shp/CITY.shp")
+    map_shp <- st_crop(map_shp, xmin = min_lon, xmax = max_lon, ymin = min_lat, ymax = max_lat)
+    return (map_shp)
 }
 
-tw_small <- LoadMap()
-df_gov <- UpdateData()
-tw_small <- tw_small[match(CityList,tw_small$COUNTYNAME),]
+TWMap <- LoadTWMap()
+df_gov <- GetGovData()
+TWMap <- TWMap[match(CityList,TWMap$COUNTYNAME),]
 
-df <- data.frame(CityList, factor(rep('未公布',22),levels = c('放假','部分上班','上班','未公布')), factor(rep('未公布',22),levels = c('放假','部分上班','上班','未公布')))
-colnames(df) <- c('City',"Today",'Tomorrow')
-dayOffChoice <-  c(放假 = 1, 部分上班 = 0.5, 上班 = 0,未公布 = -1)
+StatusVec <- c('放假','部分上班','上班','未公布')
+StatusChoice <-  c(放假 = 1, 部分上班 = 0.5, 上班 = 0,未公布 = -1)
 
-dayOffMap <- function(offstr){
-    if (offstr == '放假'){
+df_status <- data.frame(CityList, 
+                        factor(rep('未公布',22),levels = StatusVec),
+                        factor(rep('未公布',22),levels = StatusVec))
+colnames(df_status) <- c('City',"Today",'Tomorrow')
+
+
+
+StatStr2Num <- function(StatStr){
+    if (StatStr == '放假'){
         return (1)
-    }else if(offstr == '部分上班'){
+    }else if(StatStr == '部分上班'){
         return (0.5)
-    }else if(offstr == '上班'){
+    }else if(StatStr == '上班'){
         return (0)
     }else{
         return (-1)
     }
 }
-dayOffMapR <- function(offint){
-    if (offint == 1){
+StatNum2Str <- function(StatNum){
+    if (StatNum == 1){
         return ('放假')
-    }else if(offint == 0.5 ){
+    }else if(StatNum == 0.5 ){
         return ('部分上班')
-    }else if(offint == 0 ){
+    }else if(StatNum == 0 ){
         return ('上班')
     }else{
         return ("未公布")
     }
 }
 
-StringToStatus <- function(status, day){
+
+SolveStatInfo <- function(StatInfo, day = 'Today', ReturnType = 'Num'){
     if (day == 'Today'){
-        if (grepl('今天停止上班',status) && grepl('今天照常上班',status)){
-            return (0.5)
-        }else if(grepl('今天停止上班',status)){
-            return (1)
-        }else if(grepl('今天照常上班',status)){
-            return (0)
+        if (grepl('今天停止上班',StatInfo) && grepl('今天照常上班',StatInfo)){
+            StatStr <- '部分上班'
+        }else if(grepl('今天停止上班',StatInfo)){
+            StatStr <- '放假'
+        }else if(grepl('今天照常上班',StatInfo)){
+            StatStr <- '上班'
         }else{
-            return (-1)
+            StatStr <- '未公布'
         }    
     }else{
-        if (grepl('明天停止上班',status) && grepl('明天照常上班',status)){
-            return (0.5)
-        }else if(grepl('明天停止上班',status)){
-            return (1)
-        }else if(grepl('明天照常上班',status)){
-            return (0)
+        if (grepl('明天停止上班',StatInfo) && grepl('明天照常上班',StatInfo)){
+            StatStr <- '部分上班'
+        }else if(grepl('明天停止上班',StatInfo)){
+            StatStr <- '放假'
+        }else if(grepl('明天照常上班',StatInfo)){
+            StatStr <- '上班'
         }else{
-            return (-1)
+            StatStr <- '未公布'
         }    
     }
+    if (ReturnType == "Num"){
+        return(StatStr2Num(StatStr))
+    }else{
+        return(StatStr)
+    }
+        
 }
 
 
 
 for (city in df_gov$縣市名稱){
     city_status <- df_gov[df_gov$縣市名稱 == city, 2][[1]] 
-    df[df$City == city,'Today'] = dayOffMapR(StringToStatus(city_status, 'Today'))
-    df[df$City == city,'Tomorrow'] = dayOffMapR(StringToStatus(city_status, 'Tomorrow'))
+    df_status[df_status$City == city,'Today'] = SolveStatInfo(city_status, 'Today', 'Str')
+    df_status[df_status$City == city,'Tomorrow'] = SolveStatInfo(city_status, 'Tomorrow', 'Str')
 }
 
 
 ui <- basicPage(
+    # Application title
     titlePanel("放假地圖"),
     h3('資料來源：https://www.dgpa.gov.tw/typh/daily/nds.html'),
     h5('僅供程式練習及測試，實際放假請洽行政院人事行政總處。'),
     
-    # Application title
-    #titlePanel("放假地圖"),
     fluidRow(
-        # fixedRow(
-        column(width = 2,
+        column(width = 3,
                wellPanel(h4('今日'),
                          lapply(1:nrow(df), function(i) {
-                             radioButtons(paste0('Today_', i), paste0('', df[i,1]),choices = dayOffChoice, selected = dayOffMap(df[i,2]) ,inline = TRUE)
+                             radioButtons(paste0('Today_', i), paste0('', df_status[i,1]),choices = StatusChoice, selected = StatStr2Num(df_status[i,2]) ,inline = TRUE)
                          })
                )
         ),
-        column(width = 2,
+        column(width = 3,
                wellPanel(h4('明日'),
                          lapply(1:nrow(df), function(i) {
-                             radioButtons(paste0('Tomorrow_', i), paste0('', df[i,1]),choices = dayOffChoice, selected = dayOffMap(df[i,3]), inline = TRUE)
+                             radioButtons(paste0('Tomorrow_', i), paste0('', df_status[i,1]),choices = StatusChoice, selected = StatStr2Num(df_status[i,3]), inline = TRUE)
                          })
                )
         ),
@@ -137,47 +148,44 @@ ui <- basicPage(
 )
 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    color_vec <- c("#009E73",'#F0E442','#CC79A7','#999999')
-    df_react <- df
+    ColorVec <- c("#009E73",'#F0E442','#CC79A7','#999999')
+    df_react <- df_status
     makeReactiveBinding("df_react")
     newData_Today <- reactive({
-        for (i in 1:nrow(df)){
-            df[i,2] <- dayOffMapR(input[[paste0('Today_',i)]])
+        for (i in 1:nrow(df_status)){
+            df_status[i,2] <- StatNum2Str(input[[paste0('Today_',i)]])
         }
-        # df[1,2] <- dayOffMapR(input[['Today_1']])
-        # df[1,2] <- dayOffMapR(input$Today_1)
-        # df[2,2] <- dayOffMapR(input$Today_2)
-        df_react <- df
+        # df[1,2] <- StatNum2Str(input[['Today_1']])
+        # df[1,2] <- StatNum2Str(input$Today_1)
+        # df[2,2] <- StatNum2Str(input$Today_2)
+        df_react <- df_status
     })
     newData_Tomorrow <- reactive({
-        for (i in 1:nrow(df)){
-            df[i,3] <- dayOffMapR(input[[paste0('Tomorrow_',i)]])
+        for (i in 1:nrow(df_status)){
+            df_status[i,3] <- StatNum2Str(input[[paste0('Tomorrow_',i)]])
         }
-        # df[1,3] <- dayOffMapR(input[['Today_1']])
-        # df[1,3] <- dayOffMapR(input$Today_1)
-        # df[2,3] <- dayOffMapR(input$Today_2)
-        df_react <- df
+        # df[1,3] <- StatNum2Str(input[['Today_1']])
+        # df[1,3] <- StatNum2Str(input$Today_1)
+        # df[2,3] <- StatNum2Str(input$Today_2)
+        df_react <- df_status
     })
     output$Map_Today <- renderPlot({
         df_react <- newData_Today()
-        work_list <- df_react$Today
-        p <- ggplot(tw_small, aes(fill = work_list))
+        p <- ggplot(TWMap, aes(fill = df_react$Today))
         p <- p + geom_sf(color = 'black') 
         p <- p + guides(fill=guide_legend(title="放假"))
-        p <- p + scale_fill_manual(values = color_vec,drop = FALSE)
+        p <- p + scale_fill_manual(values = ColorVec,drop = FALSE)
         print(p)
     })
     
     output$Map_Tomorrow <- renderPlot({
         df_react <- newData_Tomorrow()
-        work_list <- df_react$Tomorrow
-        p <- ggplot(tw_small, aes(fill = work_list))
+        p <- ggplot(TWMap, aes(fill = df_react$Tomorrow))
         p <- p + geom_sf(color = 'black') 
         p <- p + guides(fill=guide_legend(title="放假"))
-        p <- p + scale_fill_manual(values = color_vec,drop = FALSE)
+        p <- p + scale_fill_manual(values = ColorVec,drop = FALSE)
         print(p)
     })
 }
